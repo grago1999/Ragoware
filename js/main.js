b41d28774ca964bdd547d82b29f741b8142f6002
Gianluca Rago <grago1999@gmail.com>
2017-10-05 00:36:35 -0400

    2.4.9

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const appId = 20202020;
+
+let ids = [];
+const duration = 750;
+const fileName = window.location.pathname.split("/").pop();
+const currentPage = fileName === "index.html" ? "home" : fileName.replace(".html", "");
+
+window.onblur = function () { document.title = "Hey! Come back!"; }
+window.onfocus = function () { document.title = "Who's Gianluca?"; }
+
+console.log("you found me ( ͡° ͜ʖ ͡°)");
+
+const report = (actionTypeId, elementId = null) => {
+	new Fingerprint2().get((result) => {
+		var sha = new jsSHA("SHA-512", "TEXT");
+		sha.update(result);
+		deviceId = sha.getHash("HEX");
+		var reportUrl = "https://conjungo.io/analytics/api/v1/report.php?appId="+appId.toString()+"&deviceId="+deviceId+"&actionTypeId="+actionTypeId;
+		if (elementId) {
+			reportUrl += ("&elementId=" + elementId);
+		}
+		$.get(reportUrl, (data, status) => {
+	        console.log("yay");
+	    });
+	});
+};
+
+report("view-page", currentPage);
